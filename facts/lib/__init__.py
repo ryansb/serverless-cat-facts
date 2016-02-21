dac410a5bc2957d7d09d3b68fb1b7da16acd2708
Ryan S. Brown <sb@ryansb.com>
2016-02-21 09:27:19 -0500

    add catfacts common lib for reading `catfacts.json`

--- facts/lib/__init__.py
+++ facts/lib/__init__.py
@@ -1,1 +1,10 @@
 # shared logic goes here
+import json
+import os
+
+here = os.path.dirname(os.path.realpath(__file__))
+
+def all_facts():
+    with open(os.path.join(here, '../catfacts.json')) as fact_file:
+        facts = json.load(fact_file)
+        return facts
